a0fede9fe8791b66715a3ca7f792ecaa342acc2d
Chuian <chuiantw1212@gmail.com>
2024-05-04 06:53:49 +0800

    use let

--- components/calculator/index.vue
+++ components/calculator/index.vue
@@ -365,7 +365,7 @@ async function initializeCalculator() {
     })
 }
 // 基本資料
-const profile = reactive({
+let profile = reactive({
     id: '', // 避免登入判斷錯誤
     yearOfBirth: '',
     dateOfBirth: '',
@@ -386,7 +386,7 @@ function onProfileChanged() {
     })
 }
 // 職業試算
-const career = reactive({
+let career = reactive({
     monthlyBasicSalary: 0,
     foodExpense: 3000,
     employeeWelfareFund: 0,
@@ -419,7 +419,7 @@ function onCareerChanged() {
     })
 }
 // 退休試算
-const retirement = reactive({
+let retirement = reactive({
     age: 60,
     yearToRetirement: 0,
     lifeExpectancy: 0,
@@ -456,7 +456,7 @@ function onRetirementChanged() {
     investment.period = retirement.yearToRetirement
 }
 // 投資試算
-const investment = reactive({
+let investment = reactive({
     allocationETF: '',
     irr: 0,
     stockPercentage: 0,
@@ -475,7 +475,7 @@ function onInvestmentChanged() {
     })
 }
 // 育兒試算
-const parenting = reactive({
+let parenting = reactive({
     childAnnualExpense: 0,
     spouseMonthlyContribution: 0,
     independantAge: 0,
@@ -494,7 +494,7 @@ function onParentingChanged() {
     })
 }
 // 購屋單價與總價
-const estatePrice = reactive({
+let estatePrice = reactive({
     county: '',
     town: '',
     buildingType: '',
@@ -539,7 +539,7 @@ async function onEstateBudgetChanged() {
         body: estatePrice,
     })
 }
-const estateSize = reactive({
+let estateSize = reactive({
     doubleBedRoom: 0,
     singleBedRoom: 1,
     bathroom: 0,
@@ -569,7 +569,7 @@ watch(() => estateSize, async (newValue, oldValue) => {
     }
 }, { deep: true })
 // 房屋貸款試算
-const mortgage = reactive({
+let mortgage = reactive({
     buyHouseYear: 0,
     loanPercent: 0,
     interestRate: 0,
