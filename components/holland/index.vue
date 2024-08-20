d667ead0c07ac8a5e42cc57f8906fcab6852f845
Chuian <chuiantw1212@gmail.com>
2024-08-20 11:49:06 +0800

    add keywords

--- components/holland/index.vue
+++ components/holland/index.vue
@@ -1,13 +1,86 @@
 <template>
     <el-card>
+        <template #header>
+            <div class="card-header">
+                <span>興趣職業關鍵字</span>
+            </div>
+        </template>
         <el-checkbox-group v-model="selectedValues">
-            <el-checkbox v-for="(item, index) in datasets" :key="index" :label="item.label" :value="item.value" />
+            <el-checkbox v-for="(item, index) in shuffledItems" :key="index" :label="item.label" :value="item.label"
+                @change="drawCharts()" />
         </el-checkbox-group>
+        <canvas id="hollandChart"></canvas>
     </el-card>
 </template>
 <script setup lang="ts">
-import { ref, } from 'vue'
-const selectedValues = ref([])
+import Chart from 'chart.js/auto';
+import { ref, computed, shallowRef } from 'vue'
+const selectedValues = ref<any[]>([])
+let hollandChartInstance = ref<Chart>()
+// hooks
+const shuffledItems = computed(() => {
+    return shuffle(datasets)
+})
+// methods
+function drawCharts() {
+    const hollandCodes: string[][] = selectedValues.value.map((selectedLabel: string) => {
+        console.log({
+            selectedLabel
+        })
+        const selectedItem = datasets.find(item => {
+            return item.label === selectedLabel
+        })
+        console.log({
+            selectedItem
+        })
+        return selectedItem?.value || []
+    })
+    const riasec = {
+        'R': 0,
+        'I': 0,
+        'A': 0,
+        'S': 0,
+        'E': 0,
+        'C': 0,
+    }
+    hollandCodes.forEach(value => {
+        value.forEach(code => {
+            riasec[code] += 1
+        })
+    })
+    // const datasets: {
+    //     label: string,
+    //     data: number[][],
+    //     fill: boolean,
+    //     tension: number
+    // }[] = []
+    console.log(Object(riasec))
+    const data: any = {
+        labels: ['R', 'I', 'A', 'S', 'E', 'C'],
+        datasets: [{
+            label: '興趣何綸碼',
+            data: Object.values(riasec),
+        }],
+    }
+    if (hollandChartInstance.value) {
+        hollandChartInstance.value.data = data
+        hollandChartInstance.value.update()
+        return
+    }
+    const ctx: any = document.getElementById('hollandChart')
+    const chartInstance = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+    })
+    hollandChartInstance = shallowRef(chartInstance) as any
+}
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+}
 const datasets = [
     {
         label: '機械/電子',
@@ -110,7 +183,7 @@ const datasets = [
         value: ['S']
     },
     {
-        label: '社會服務',
+        label: '社會服務/公益',
         value: ['S']
     },
     {
