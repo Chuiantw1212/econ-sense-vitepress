55cfea53a0d5c7160ec50d7d0b4ed3bf1eef8e5a
Chuian <chuiantw1212@gmail.com>
2024-08-23 13:33:31 +0800

    clear code

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,16 +1,9 @@
-// https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import { install } from 'element-plus'
 import 'element-plus/dist/index.css'
 import './custom.css'
 export default {
   extends: Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
   async enhanceApp({ app, }) {
     install(app)
   }
