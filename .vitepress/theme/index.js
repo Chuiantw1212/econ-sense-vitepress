03e9acafc19b07c56cb40ce756593807e1f79852
Chuian <chuiantw1212@gmail.com>
2024-12-06 14:09:34 +0800

    test

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -15,17 +15,19 @@ import firebase from 'firebase/compat/app'
 import "firebase/compat/performance";
 
 // Firebase
-window.firebase = firebase
-firebase.initializeApp({
-  apiKey: "AIzaSyDzxiXnAvtkAW5AzoV-CsBLNbryVJZrGqI",
-  authDomain: "econ-sense-9a250.firebaseapp.com",
-  projectId: "econ-sense-9a250",
-  storageBucket: "econ-sense-9a250.appspot.com",
-  messagingSenderId: "449033690264",
-  appId: "1:449033690264:web:f5e419118030eb3afe44ed",
-  measurementId: "G-19NFT8GVCZ"
-})
-firebase.performance()
+if (window) {
+  window.firebase = firebase
+  firebase.initializeApp({
+    apiKey: "AIzaSyDzxiXnAvtkAW5AzoV-CsBLNbryVJZrGqI",
+    authDomain: "econ-sense-9a250.firebaseapp.com",
+    projectId: "econ-sense-9a250",
+    storageBucket: "econ-sense-9a250.appspot.com",
+    messagingSenderId: "449033690264",
+    appId: "1:449033690264:web:f5e419118030eb3afe44ed",
+    measurementId: "G-19NFT8GVCZ"
+  })
+  firebase.performance()
+}
 
 // export vue app
 export default {
