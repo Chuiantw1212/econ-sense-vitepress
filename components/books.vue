71eab8fa0d068faad5e5ac873e58993e68e7e24f
Chuian <chuiantw1212@gmail.com>
2025-01-17 16:17:48 +0800

    wip

--- components/books.vue
+++ components/books.vue
@@ -5,11 +5,9 @@
                 {{ item.name }}
             </h3>
             <div class="book__body">
-                <!-- <a class="book__link" :href="getApredir(item)" target="_blank"> -->
                 <div class="book__link">
                     <img class="link__image" loading="lazy" :src="getImageLink(item)" alt="封面">
                 </div>
-                <!-- </a> -->
                 <div class="book__textGroup">
                     <div v-show="item.desc" v-html="item.desc">
                     </div>
