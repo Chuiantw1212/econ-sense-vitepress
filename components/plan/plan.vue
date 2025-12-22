6f45204acab5ddb4e7bb82f6f436b42962c330b8
EN Chu <chuiantw1212@gmail.com>
2025-12-22 15:34:38 +0800

    able to put

--- components/plan/plan.vue
+++ components/plan/plan.vue
@@ -39,10 +39,10 @@ const initData = async () => {
 }
 
 async function onProfileChanged() {
-    // authFetch(`/plan/profile`, {
-    //     method: 'put',
-    //     body: userForm.value.profile,
-    // })
+    authFetch(`/api/v1/user/profile`, {
+        method: 'PUT',
+        body: userForm.value.profile,
+    })
     // changeAllCards({
     //     profile: true
     // })
