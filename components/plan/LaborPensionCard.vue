cc0682bb5e11ef99619b2a66de4310a35d057d44
EN Chu <chuiantw1212@gmail.com>
2026-08-14 07:29:54 +0800

    wip

--- components/plan/LaborPensionCard.vue
+++ components/plan/LaborPensionCard.vue
@@ -27,14 +27,14 @@
                     </el-form-item>
                 </el-col>
                 <el-col :span="12" :xs="24">
-                    <el-form-item label="雇主提繳-累計收益">
-                        <el-input-number v-model="model.laborPension.employerEarnings" :min="0" :step="5000"
-                            style="width: 100%" />
+                    <el-form-item label="個人自提-累計金額">
+                        <el-input-number v-model="model.laborPension.personalContribution" :min="0" :step="10000"
+                        style="width: 100%" />
                     </el-form-item>
                 </el-col>
                 <el-col :span="12" :xs="24">
-                    <el-form-item label="個人自提-累計金額">
-                        <el-input-number v-model="model.laborPension.personalContribution" :min="0" :step="10000"
+                    <el-form-item label="雇主提繳-累計收益">
+                        <el-input-number v-model="model.laborPension.employerEarnings" :min="0" :step="5000"
                             style="width: 100%" />
                     </el-form-item>
                 </el-col>
